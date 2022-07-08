1d23031a890499cc3f171c93719d19b3109a7769
jgcodes2020 <jackyguo18@hotmail.com>
2022-07-08 15:06:40 -0400

    Gave TASInput an icon

--- src/tasinput-qt/src/main.cpp
+++ src/tasinput-qt/src/main.cpp
@@ -1,7 +1,12 @@
 #include <fmt/core.h>
+#include <qdir.h>
+#include <qdiriterator.h>
 #include <QApplication>
+#include <QDirIterator>
+#include <QIcon>
 #include <QMainWindow>
 #include <QMetaObject>
+#include <QStandardPaths>
 #include <Qt>
 
 #include <any>
@@ -25,7 +30,41 @@
 
 #define ERR(str) ("ERR:" str)
 
-#define ERR(str) ("ERR:" str)
+void set_icon_theme() {
+  if (qApp->platformName() == "wayland") {
+    // Wayland is special. Copy the icon theme into the theme folders.
+    QDir local_share =
+      QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation);
+    QDir res_share(":/res/share");
+
+    QDirIterator qdi(res_share, QDirIterator::Subdirectories);
+    while (qdi.hasNext()) {
+      QFileInfo f = qdi.nextFileInfo();
+
+      if (f.isDir()) {
+        local_share.mkpath(res_share.relativeFilePath(f.filePath()));
+      }
+      else if (f.isFile()) {
+        QString dst = local_share.path() + QDir::separator() +
+          res_share.relativeFilePath(f.filePath());
+        QFile::copy(f.filePath(), dst);
+        QFile::setPermissions(dst, QFile::Permissions(0x6644));
+      }
+    }
+  }
+  else {
+    QIcon icon(":res/share/icons/hicolor/128x128/apps/io.github.jgcodes.tasinput-qt.png");
+    for (auto size : {16, 32, 38, 64, 128}) {
+      QString path =
+        QStringLiteral(
+          ":res/share/icons/hicolor/%1x%1/apps/io.github.jgcodes.tasinput-qt.png")
+          .arg(QString::number(size));
+          
+      icon.addFile(path, QSize(size, size));
+    }
+    tnp::w->setWindowIcon(icon);
+  }
+}
 
 int main(int argc, char* argv[]) {
   if (argc < 2)
@@ -43,25 +82,30 @@ int main(int argc, char* argv[]) {
   a.setDesktopFileName("io.github.jgcodes.tasinput-qt");
 
   tnp::w = decltype(tnp::w)(new tnp::MainWindow());
-  
+  set_icon_theme();
 
   std::thread postOffice([&]() {
     bool stop_flag = true;
     while (true) {
-      auto data = tnp::app_shm_client->ipc_data().pull(&tnp::ipc_layout::mq_p2e);
-      std::visit(tnp::overload {
-        [&stop_flag](const tnp::ipc::MessageQuery& query) -> void {
-          tnp::ipc::MessageReply reply;
-          tnp::app_server->handle_request(query, reply);
-          tnp::app_shm_client->ipc_data().mq_e2p.emplace(reply);
-          
-          if (query.method() == "QuitApp" && query.service() == "tnp.prtc.AppService") {
-            stop_flag = false;
-          }
-        },
-        [](const tnp::ipc::MessageReply&) -> void {}
-      }, data);
-      
+      auto data =
+        tnp::app_shm_client->ipc_data().pull(&tnp::ipc_layout::mq_p2e);
+      std::visit(
+        tnp::overload {
+          [&stop_flag](const tnp::ipc::MessageQuery& query) -> void {
+            tnp::ipc::MessageReply reply;
+            tnp::app_server->handle_request(query, reply);
+            tnp::app_shm_client->ipc_data().mq_e2p.emplace(reply);
+
+            if (
+              query.method() == "QuitApp" &&
+              query.service() == "tnp.prtc.AppService") {
+              stop_flag = false;
+            }
+          },
+          [](const tnp::ipc::MessageReply&) -> void {
+          }},
+        data);
+
       if (!stop_flag && tnp::app_shm_client->ipc_data().mq_p2e.empty())
         break;
     }
